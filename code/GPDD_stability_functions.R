510ca3685ddaacf392ab1aaf413318dd5100e83b
Tanya Rogers <tanya.l.rogers@gmail.com>
2020-09-04 18:37:51 -0700

    add make_block original

--- code/GPDD_stability_functions.R
+++ code/GPDD_stability_functions.R
@@ -539,3 +539,58 @@ timescale_mo=function(SamplingInterval, y) {
                                      ifelse(SamplingInterval=="weekly", y/4.286,NA))))))
   
 }
+
+#old make_block function, for compatibility
+make_block=function (block, t = NULL, max_lag = 3, tau = 1, lib = NULL, 
+                     restrict_to_lib = TRUE) 
+{
+  if (is.vector(block)) 
+    block <- matrix(block, ncol = 1)
+  num_vars <- NCOL(block)
+  num_rows <- NROW(block)
+  if (!is.null(lib)) {
+    if (is.vector(lib)) 
+      lib <- matrix(lib, ncol = 2, byrow = TRUE)
+  }
+  output <- matrix(NA, nrow = num_rows, ncol = 1 + num_vars * 
+                     max_lag)
+  col_names <- character(1 + num_vars * max_lag)
+  if (is.null(t)) 
+    output[, 1] <- 1:num_rows
+  else output[, 1] <- t
+  col_names[1] <- "time"
+  col_index <- 2
+  if (is.null(colnames(block))) 
+    colnames(block) <- paste0("col", seq_len(num_vars))
+  for (j in 1:num_vars) {
+    ts <- block[, j]
+    if (is.list(ts)) {
+      ts <- unlist(ts)
+    }
+    output[, col_index] <- ts
+    col_names[col_index] <- colnames(block)[j]
+    col_index <- col_index + 1
+    if (max_lag > 1) {
+      for (i in 1:(max_lag - 1)) {
+        ts <- c(rep_len(NA, tau), ts[1:(num_rows - tau)])
+        if (!is.null(lib)) {
+          for (k in seq_len(NROW(lib))) {
+            ts[lib[k, 1] - 1 + (1:tau)] <- NA
+          }
+        }
+        output[, col_index] <- ts
+        col_names[col_index] <- paste0(colnames(block)[j], 
+                                       "_", i * tau)
+        col_index <- col_index + 1
+      }
+    }
+  }
+  if (!is.null(lib) && restrict_to_lib) {
+    row_idx <- sort(unique(do.call(c, mapply(seq, lib[, 1], 
+                                             lib[, 2], SIMPLIFY = FALSE))))
+    output <- output[row_idx, ]
+  }
+  output <- data.frame(output)
+  names(output) <- col_names
+  return(output)
+}
