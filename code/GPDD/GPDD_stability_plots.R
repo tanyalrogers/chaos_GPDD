cc775db29b59d80bb57e282f4fce492cbeb897fb
Tanya Rogers <tanya.l.rogers@gmail.com>
2022-04-08 10:53:09 -0700

    add le histo, clean up fig names

--- code/GPDD/GPDD_stability_plots.R
+++ code/GPDD/GPDD_stability_plots.R
@@ -59,7 +59,7 @@ ggplot(filter(gpdd_d, E>1), aes(y=LEmean1d, x=LEmean, fill=factor(E))) +
   geom_point(size=3, pch=21, color="black", alpha=0.9) +
   geom_hline(yintercept = 0) + geom_vline(xintercept = 0) + scale_fill_viridis_d() +
   classic + labs(x="LE, Free E", y="LE, E = 1", fill="Free E")
-ggsave("./figures/LE1dvsnd2.png", width = 4, height = 3)
+ggsave("./figures/LE1dvsnd.png", width = 4, height = 3)
 
 #distribution of Es, taus ####
 Es=ggplot(gpdd_d, aes(x=factor(E), fill=TaxonomicClass3)) + 
@@ -130,7 +130,7 @@ lemass1=ggplot(posLE, aes(y=log10(LEmean), x=log10(Mass_g))) +
   guides(fill = guide_legend(override.aes = list(size = 3.5, shape=21))) +
   guides(shape = guide_legend(override.aes = list(stroke = c(0.5,0.5,1.25)))) +
   theme(axis.title = element_text(size=12), axis.text = element_text(size=10))
-ggsave("./figures/mass_scaling4.png", lemass1, width = 5.25, height = 4)
+ggsave("./figures/mass_scaling.png", lemass1, width = 5.25, height = 4)
 
 lemass2=ggplot(posLE, aes(y=log10(LEmean), x=log10(Mass_g))) + 
   ylab(expression(~log[10]~LE~(month^-1))) + xlab(expression(~log[10]~Mass~(g))) +
@@ -177,7 +177,7 @@ LEmgtE=ggplot(gpdd_d, aes(y=LEmean_mo, x=log10(MinAge_mo), color=E)) +
 left=plot_grid(classgtE + theme(legend.position="none"), LEmgtE + theme(legend.position="none"), nrow = 1, labels=c("A","B"))
 legend <- get_legend(LEmgtE + theme(legend.box.margin = margin(0, 0, 0, 3)))
 plot_grid(left,legend, ncol = 2, rel_widths = c(1,0.1))
-ggsave("./figures/gentime2.png", width = 7, height = 3)
+ggsave("./figures/gentime.png", width = 7, height = 3)
 
 mod=glm(LEclass01~log10(MinAge_mo), data=gpdd_d, family="binomial")
 car::Anova(mod)
@@ -230,7 +230,7 @@ r22=ggplot(gpdd_d, aes(y=R2abund, x=R2gr, fill=LEclass)) +
 r2plots=plot_grid(r2class + theme(legend.position="none"), r2ab + theme(legend.position="none"), nrow = 1, labels=c("A","B"))
 legend <- get_legend(r2class + theme(legend.box.margin = margin(0, 0, 0, 3)))
 r2plots1=plot_grid(r2plots,legend, ncol = 2, rel_widths = c(1,0.25))
-ggsave("./figures/R2LE.png", r2plots1, width = 7, height = 3)
+#ggsave("./figures/R2LE.png", r2plots1, width = 7, height = 3)
 
 r2plots=plot_grid(r2ab + theme(legend.position="none"), r2gr + theme(legend.position="none"), align = 'vh',labels="AUTO")
 legend <- get_legend(r2ab + theme(legend.box.margin = margin(0, 0, 0, 12)))
@@ -260,7 +260,7 @@ mtle=ggplot(gpdd_d, aes(y=LEmean_mo, x=monotonicR2, fill=TaxonomicClass3)) +
 mtplots=plot_grid(mtclass + theme(legend.position="none"), mtle + theme(legend.position="none"), nrow = 1, labels=c("C","D"))
 legend <- get_legend(mtclass + theme(legend.box.margin = margin(0, 0, 0, 3)))
 mtplots1=plot_grid(mtplots,legend, ncol = 2, rel_widths = c(1,0.25))
-#ggsave("./figures/monotonictrend2.png", mtplots1, width = 7, height = 3)
+#ggsave("./figures/monotonictrend.png", mtplots1, width = 7, height = 3)
 
 #combined R2 and monotonic trend plot for paper
 plot_grid(r2plots1,mtplots1,ncol=1)
@@ -344,6 +344,7 @@ plot_grid(hCV,hR2,htheta,hmono,ncol = 2, align = "vh", labels = "AUTO")
 ggsave("./figures/histograms.png", width = 8, height = 6)
 
 # restrict ts length and aggregate by site ####
+# for site level, classify using median and max
 
 gpdd_sub50=filter(gpdd_d,ndatapoints>=50)
 gpdd_sub70=filter(gpdd_d,ndatapoints>=70)
@@ -392,6 +393,7 @@ tstable$psitemax[4]=length(which(gpdd_avg70b$LEmax>0.01))/length(which(!is.na(gp
 tstable[,4:6]=round(tstable[,4:6],2)
 tstable
 
+# corrected site level prob chaos #### 
 gpdd_sitetax=gpdd_d %>% group_by(ExactName) %>% 
   summarise(sitetax=paste0(sort(unique(TaxonomicClass3)),collapse = ", "))
 gpdd_prop=gpdd_d %>% group_by(ExactName) %>% 
@@ -422,9 +424,9 @@ p2=ggplot(gpdd_prop, aes(x=propcorrected, fill=sitetax)) +
 pcplots=plot_grid(p1 + theme(legend.position="none"), p2 + theme(legend.position="none"), nrow = 1, labels=c("A","B"))
 legend <- get_legend(p1 + theme(legend.box.margin = margin(0, 0, 0, 3), legend.text = element_text(size=8)))
 plot_grid(pcplots,legend, ncol = 2, rel_widths = c(1,0.3))
-ggsave("./figures/siteprobchaos2.png", width = 8, height = 3)
+ggsave("./figures/histo_siteprobchaos.png", width = 8, height = 3)
 
-#by taxa
+# prob chaos by taxon
 gpdd_avg = gpdd_avg %>% left_join(gpdd_sitetax)
 gpdd_avg %>% group_by(sitetax) %>% 
   summarise(n=n(), chaotic = length(which(LEmedian>0.01)), propchaotic=chaotic/n) %>% 
@@ -433,12 +435,25 @@ gpdd_avg %>% group_by(sitetax) %>%
 #lakes
 lakes2 %>% group_by(Site) %>% 
   summarise(LEmedian=median(JLE),
-            LEmedian>0.01,
             nseries=n(),
             propchaotic=length(which(JLEsign=="chaotic"))/nseries,
             propcorrected=min(max(0,((propchaotic-0.04)/(0.71-0.04))),1))
 
-# time series plots ####
+# histograms for sites with >8 time series ####
+
+lehisto=filter(gpdd_d, ExactName %in% c("Port Erin Bay","William Trelease Woods","Hinkley Point"))
+lehisto$TaxonomicClass3=factor(lehisto$TaxonomicClass3,levels = sort(unique(gpdd_d$TaxonomicClass3)))
+ggplot(lehisto,aes(x=LEmean_mo,fill=TaxonomicClass3)) +
+  facet_wrap(.~ExactName, scale="free") +
+  geom_histogram(bins=25) +
+  geom_vline(aes(xintercept=0)) +
+  classic + labs(x=expression(LE~(month^-1)),y="Count", fill="Taxonomic\nGroup") +
+  scale_y_continuous(expand = expand_scale(mult = c(0, 0.05))) +
+  scale_fill_brewer(palette = "Dark2", drop=F) +
+  removefacetbackground
+ggsave("./figures/lehistos.png", width = 8, height = 3)
+
+# sample time series plots ####
 
 gpdd_d_ts=read.csv("./data/gpdd_timeseries.csv",stringsAsFactors = F)
 
